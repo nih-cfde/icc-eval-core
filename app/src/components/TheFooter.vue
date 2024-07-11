471ad2473704d56bb69b3b7878243c34923f5d1b
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-07-11 11:32:38 -0400

    tweak type checking

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -45,13 +45,16 @@ onMounted(async () => {
      * get list of pdf files. safely import dynamically to not fail build if
      * import does not exist.
      */
-    pdfs.value = (
-      await Object.values(
-        import.meta.glob<boolean, string, { default: Record<string, string> }>(
-          "~/pdfs.json",
-        ),
-      )[0]()
-    ).default;
+    pdfs.value =
+      (
+        await Object.values(
+          import.meta.glob<
+            boolean,
+            string,
+            { default: Record<string, string> }
+          >("~/pdfs.json"),
+        )[0]?.()
+      )?.default ?? {};
   } catch (error) {
     console.error("Couldn't load PDF list");
   }
