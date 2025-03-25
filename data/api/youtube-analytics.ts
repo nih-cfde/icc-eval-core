14e7f45fbb415f171128dceca2dffe10e96f6210
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-03-24 21:17:09 -0400

    install

---
+++ data/api/youtube-analytics.ts
@@ -0,0 +1,5 @@
+import { youtube } from "@googleapis/youtube";
+import { youtubeAnalytics } from "@googleapis/youtubeanalytics";
+
+const dataClient = youtube("v3");
+const analyticsClient = youtubeAnalytics("v2");
