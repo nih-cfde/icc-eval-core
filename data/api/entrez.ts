8c22961a3abaa8407468c2a58fbf59df10fefc0e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-12-11 13:11:15 -0500

    remove useless try catch

--- data/api/entrez.ts
+++ data/api/entrez.ts
@@ -11,26 +11,22 @@ const { AUTH_ENTREZ = "" } = process.env;
 export const getFullJournalName = memoize(async (abbreviated: string) => {
   /** https://stackoverflow.com/questions/75512796/how-do-i-get-issns-and-full-journal-names-from-a-list-of-abbreviated-journal-nam */
 
-  try {
-    /** common params */
-    const params = { retmode: "json", db: "pubmed", api_key: AUTH_ENTREZ };
-    /** get papers associated with journal abbreviation */
-    const searchResults = await request<EsearchResults>(esearch, {
-      params: { ...params, term: `${abbreviated}[TA]` },
-    });
-    /** get first paper id */
-    const id = searchResults.esearchresult.idlist?.[0] ?? "";
-    /** look up full paper details */
-    const summaryResults = await request<EsummaryResults>(esummary, {
-      params: { ...params, id },
-    });
-    /** get full journal name */
-    const name = summaryResults.result?.[id]?.fulljournalname ?? "";
-    /** get issn */
-    const issn = summaryResults.result?.[id]?.essn?.replaceAll("-", "") ?? "";
+  /** common params */
+  const params = { retmode: "json", db: "pubmed", api_key: AUTH_ENTREZ };
+  /** get papers associated with journal abbreviation */
+  const searchResults = await request<EsearchResults>(esearch, {
+    params: { ...params, term: `${abbreviated}[TA]` },
+  });
+  /** get first paper id */
+  const id = searchResults.esearchresult.idlist?.[0] ?? "";
+  /** look up full paper details */
+  const summaryResults = await request<EsummaryResults>(esummary, {
+    params: { ...params, id },
+  });
+  /** get full journal name */
+  const name = summaryResults.result?.[id]?.fulljournalname ?? "";
+  /** get issn */
+  const issn = summaryResults.result?.[id]?.essn?.replaceAll("-", "") ?? "";
 
-    return { name, issn };
-  } catch (error) {
-    console.error(error);
-  }
+  return { name, issn };
 });
