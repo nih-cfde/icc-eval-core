792d95a04d33c7749d7bbc43035dd779110f1d34
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2026-05-27 10:45:53 -0600

    Frontend using mock vs. real data settable via env var VITE_USE_MOCK

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -14,10 +14,11 @@ import publications from "./types/publications.json";
 import repoOverview from "./types/repo-overview.json";
 import repos from "./types/repos.json";
 
-const { VITE_API: api } = import.meta.env;
+const { VITE_API: api, VITE_USE_MOCK: useMock } = import.meta.env;
 
-/** whether to use placeholder data */
-const mock = true;
+/** whether to use fake data */
+// if useMock is not set, default to true
+const mock = useMock === undefined ? true : useMock === "true";
 
 /** link to SSO login */
 export const loginLink = `${api}/accounts/orcid/login/`;
