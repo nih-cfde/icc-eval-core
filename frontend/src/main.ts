a244969cebd2dc776aa5587e65faaff72057444e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-07-09 16:50:35 -0400

    remove frontend cliend-side redirect

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,8 +9,6 @@ import PageDrc from "./pages/PageDrc.vue";
 import PageHome from "./pages/PageHome.vue";
 import "./styles.css";
 
-const domain = "metrics.cfdeconnect.org";
-
 /** app pages */
 const routes = [
   {
@@ -23,12 +21,6 @@ const routes = [
       /** get redirect path from 404.html */
       const redirect = window.sessionStorage.redirect as string;
       window.sessionStorage.removeItem("redirect");
-
-      /** redirect to new domain */
-      if (window.location.hostname !== domain) {
-        window.location.href = new URL(redirect, `https://${domain}`).href;
-        return;
-      }
 
       /** redirect to path */
       if (redirect) {
